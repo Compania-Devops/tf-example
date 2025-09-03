349def74acee99ecc5d4e298516923c925bc3b3a
bac2795 <69336954+bac2795@users.noreply.github.com>
2025-09-03 00:41:55 -0500

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,3 +1,27 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+}
 terraform {
   backend "azurerm" {
     resource_group_name   = "rg-cicd-terraform-app-apellido" # Reemplazar por apellido
