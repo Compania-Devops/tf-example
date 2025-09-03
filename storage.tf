25456c09caa68bfbfe6adf263e61f5915f645179
bac2795 <69336954+bac2795@users.noreply.github.com>
2025-09-03 00:44:55 -0500

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -22,10 +22,11 @@ variable "client_secret" {
 variable "tenant_id" {
   type = string
 }
+
 terraform {
   backend "azurerm" {
     resource_group_name   = "rg-cicd-terraform-app-apellido" # Reemplazar por apellido
-    storage_account_name  = "tfstateapellido}" # Reemplazar por apellido
+    storage_account_name  = "tfstateapellido" # Reemplazar por apellido
     container_name        = "tfstate"
     key                   = "dev/terraform.tfstate"
   }
